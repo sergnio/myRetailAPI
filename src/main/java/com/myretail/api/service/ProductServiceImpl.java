eecc9668e4c0764b9ee010da74980d9e27c2c1aa
Sergnio <sergnio@gmail.com>
2019-01-14 22:06:19 -0600

    Added basic test class Added spring cloud app id in props file Minor refactor, code cleanup

--- src/main/java/com/myretail/api/service/ProductServiceImpl.java
+++ src/main/java/com/myretail/api/service/ProductServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
     @Autowired
     private ProductRepository productRepository;
 
---
+++ src/test/java/com/myretail/api/ApiTest.java
@@ -0,0 +1,12 @@
+package com.myretail.api;
+
+
+import org.junit.Test;
+
+public class ApiTest {
+
+    @Test
+    public void test() {
+
+    }
+}
